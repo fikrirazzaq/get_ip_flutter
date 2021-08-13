5aafa828ac4794ec0ee56803cb351c39c66b3743
Fikri <fikri.arrasyid@fnbees.com>
2021-08-13 11:13:13 +0700

    migrate: null-safety.

--- lib/get_ip.dart
+++ lib/get_ip.dart
@@ -9,8 +9,8 @@ class GetIp {
   /// Returns the IPv4-Address the device is aware of
   /// (depending on your network configuration)
   /// Can be null
-  static Future<String> get ipAddress async {
-    final String ip = await _channel.invokeMethod('getIpAdress');
+  static Future<String?> get ipAddress async {
+    final String? ip = await _channel.invokeMethod('getIpAdress');
     return ip;
   }
 
@@ -18,8 +18,8 @@ class GetIp {
   /// Returns the users IPv6-Address the device is aware of
   /// (depending on your network configuration)
   /// Is null on iOS
-  static Future<String> get ipv6Address async {
-    final String ip = await _channel.invokeMethod('getIpV6Adress');
+  static Future<String?> get ipv6Address async {
+    final String? ip = await _channel.invokeMethod('getIpV6Adress');
     return ip;
   }
 }
